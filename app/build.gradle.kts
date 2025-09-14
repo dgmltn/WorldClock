5409e317ab04055fa4badf540d6b1fbbb155ca77
Doug Melton <what@supbro.com>
2025-09-14 13:46:52 -0700

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.dgmltn.worldclock"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.dgmltn.worldclock"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
